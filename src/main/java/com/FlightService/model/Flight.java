29da718037e4f8fe215eb555c71ad7a04822753f
Vignesh Raghav <vigneshraghav@Vigneshs-MacBook-Air.local>
2024-11-11 02:39:08 -0500

    Initial Commit

---
+++ src/main/java/com/FlightService/model/Flight.java
@@ -0,0 +1,76 @@
+package com.FlightService.model;
+
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public class Flight {
+
+    @Id
+    private int flightNumber;
+    private String airline;
+    private String origin;
+    private String destination;
+    private LocalDateTime scheduledDepartureTime;
+    private LocalDateTime scheduledArrivalTime;
+    private String aircraftType;
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDateTime getScheduledDepartureTime() {
+        return scheduledDepartureTime;
+    }
+
+    public void setScheduledDepartureTime(LocalDateTime scheduledDepartureTime) {
+        this.scheduledDepartureTime = scheduledDepartureTime;
+    }
+
+    public LocalDateTime getScheduledArrivalTime() {
+        return scheduledArrivalTime;
+    }
+
+    public void setScheduledArrivalTime(LocalDateTime scheduledArrivalTime) {
+        this.scheduledArrivalTime = scheduledArrivalTime;
+    }
+
+    public String getAircraftType() {
+        return aircraftType;
+    }
+
+    public void setAircraftType(String aircraftType) {
+        this.aircraftType = aircraftType;
+    }
+
+
+
+}
